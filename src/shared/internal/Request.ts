b5f8a9631d56d353e3848f4f3e6aa11bc92ef17f
Remi S <rmtlobbys@icloud.com>
2022-01-18 14:21:18 -0800

    Rating now a part of Danbooru random image search

--- src/shared/internal/Request.ts
+++ src/shared/internal/Request.ts
@@ -7,15 +7,23 @@ type DanbooruRating = 's' | 'q' | 'e';
 
 export default class Request {
 
+    /**
+     * Gets a random image from danbooru.
+     * @param {string} [tags] - The tags to search for.
+     * @param {DanbooruRating} [rating] - The rating of the image.
+     * @returns The JSON object containing the image data.
+     */
     public static async getRandomDanbooruImage(tags?: string, rating?: DanbooruRating): Promise<IDanbooruAPIResponse> {
         let url: string = "https://danbooru.donmai.us/posts.json?random=true&limit=1";
 
-        if (tags || tags !== '') {
+        if (tags || tags !== '') 
             url += `&tags=${tags}`;
-        }
+
+        if(rating || rating !== '')
+            url += `&rating=${rating}`;
+
         let { body } = await got(url);
         return JSON.parse(body)[0] as IDanbooruAPIResponse;
-
     }
 
     /*public static async getLivechartLinks(): Promise<void> {
